fd1fa64aaf678f796391a189d02540d0363d6aea
Paul Barrett <paulg.barrett4@gmail.com>
2022-03-09 12:54:35 -0500

    add instructions and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,27 @@ export class AppComponent implements OnInit {
   gameState: GameData | undefined;
 
   @HostListener('document:keydown', ['$event'])
-  handleKeyboardEvent(event: KeyboardEvent) {
+  @HostListener('touchstart', ['$event'])
+  @HostListener('touchend', ['$event'])
+  handleKeyboardOrSwipeEvent(event: any) {
     if (this.logicService.isGameOver()) return;
+    console.error('=============> event: ', event.type);
+    if (event.type === 'keydown') {
+      this.handleKeyBoardEvent(event);
+    }
+    if (event.type === 'touchstart' || event.type === 'touchend') {
+      this.handleSwipeEvent(event);
+    }
+  }
+
+  constructor(public logicService: LogicService) {
+  }
+
+  ngOnInit() {
+    this.gameState = this.logicService.startNewGame();
+  }
+
+  handleKeyBoardEvent(event: KeyboardEvent) {
     switch (event.key) {
       case 'ArrowRight':
         this.gameState = this.logicService.moveRight(this.logicService.boardState);
@@ -30,11 +49,22 @@ export class AppComponent implements OnInit {
     }
   }
 
-  constructor(public logicService: LogicService) {
-  }
-
-  ngOnInit() {
-    this.gameState = this.logicService.startNewGame();
+  handleSwipeEvent(event: TouchEvent) {
+    console.error('=============> event: ', event);
+    // switch (event.key) {
+    //   case 'ArrowRight':
+    //     this.gameState = this.logicService.moveRight(this.logicService.boardState);
+    //     break;
+    //   case 'ArrowLeft':
+    //     this.gameState = this.logicService.moveLeft();
+    //     break;
+    //   case 'ArrowUp':
+    //     this.gameState = this.logicService.moveUp();
+    //     break;
+    //   case 'ArrowDown':
+    //     this.gameState = this.logicService.moveDown();
+    //     break;
+    // }
   }
 
 }
