7533f97aeb8fa2dad7068ce4670713d8c99ec5d7
Paul Barrett <paulg.barrett4@gmail.com>
2022-03-11 12:38:10 -0500

    add touch controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ import {GameData} from './models/GameData';
 export class AppComponent implements OnInit {
 
   gameState: GameData | undefined;
+  defaultTouch = { x: 0, y: 0, time: 0 };
 
   @HostListener('document:keydown', ['$event'])
   @HostListener('touchstart', ['$event'])
   @HostListener('touchend', ['$event'])
   handleKeyboardOrSwipeEvent(event: any) {
     if (this.logicService.isGameOver()) return;
-    console.error('=============> event: ', event.type);
     if (event.type === 'keydown') {
       this.handleKeyBoardEvent(event);
     }
@@ -50,21 +50,40 @@ export class AppComponent implements OnInit {
   }
 
   handleSwipeEvent(event: TouchEvent) {
-    console.error('=============> event: ', event);
-    // switch (event.key) {
-    //   case 'ArrowRight':
-    //     this.gameState = this.logicService.moveRight(this.logicService.boardState);
-    //     break;
-    //   case 'ArrowLeft':
-    //     this.gameState = this.logicService.moveLeft();
-    //     break;
-    //   case 'ArrowUp':
-    //     this.gameState = this.logicService.moveUp();
-    //     break;
-    //   case 'ArrowDown':
-    //     this.gameState = this.logicService.moveDown();
-    //     break;
-    // }
+    let touch = event.touches[0] || event.changedTouches[0];
+
+    if (event.type === 'touchstart') {
+      this.defaultTouch = {
+        x: touch.pageX,
+        y: touch.pageY,
+        time: event.timeStamp
+      };
+    } else if (event.type === 'touchend') {
+      let deltaX = touch.pageX - this.defaultTouch.x;
+      let deltaY = touch.pageY - this.defaultTouch.y;
+      let deltaTime = event.timeStamp - this.defaultTouch.time;
+
+      if (deltaTime < 500) {
+        // touch movement lasted less than 500 ms
+        if (Math.abs(deltaX) > 60) {
+          // delta x is at least 60 pixels
+          if (deltaX > 0) {
+            this.gameState = this.logicService.moveRight(this.logicService.boardState);
+          } else {
+            this.gameState = this.logicService.moveLeft();
+          }
+        }
+
+        if (Math.abs(deltaY) > 60) {
+          // delta y is at least 60 pixels
+          if (deltaY > 0) {
+            this.gameState = this.logicService.moveDown();
+          } else {
+            this.gameState = this.logicService.moveUp();
+          }
+        }
+      }
+    }
   }
 
 }
